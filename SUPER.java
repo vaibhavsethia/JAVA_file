30be8d5e46dc4fabcc5127ed111c3d285a6d9d06
IrateLeaf98 <40268761+vaibhavsethia@users.noreply.github.com>
2019-03-19 14:37:34 +0530

    Usage of super keyword

---
+++ SUPER.java
@@ -0,0 +1,34 @@
+
+class Person 
+{ 
+    void message() 
+    { 
+        System.out.println("This is person class"); 
+    } 
+} 
+
+  
+class Student extends Person 
+{ 
+    void message() 
+    { 
+        System.out.println("This is student class"); 
+  
+    } 
+  
+    void display() 
+    { 
+  
+        message(); 
+  
+        super.message(); 
+    } 
+} 
+public class SUPER 
+{ 
+    public static void main(String args[]) 
+    { 
+        Student s = new Student(); 
+        s.display(); 
+    } 
+} 
